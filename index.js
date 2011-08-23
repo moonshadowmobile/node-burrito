d6ca701a474ecf202ad61bb76baba9d730fc86cd
James Halliday <mail@substack.net>
2011-08-23 06:54:52 -0700

    isArray fix, now works in all the IEs

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ var traverse = require('traverse');
 var vm = require('vm');
 
 var burrito = module.exports = function (code, cb) {
-    var ast = Array.isArray(code)
+    var ast = Array_isArray(code)
         ? code // already an ast
         : parse(code.toString(), false, true)
     ;
@@ -51,7 +51,7 @@ var burrito = module.exports = function (code, cb) {
 var wrapNode = burrito.wrapNode = function (state, cb) {
     var node = state.node;
     
-    var ann = Array.isArray(node) && node[0]
+    var ann = Array_isArray(node) && node[0]
     && typeof node[0] === 'object' && node[0].name
         ? node[0]
         : null
@@ -198,4 +198,8 @@ burrito.label = function (node) {
     else {
         return null;
     }
+};
+
+var Array_isArray = Array.isArray || function isArray (xs) {
+    return Object.prototype.toString.call(xs) === '[object Array]';
 };
